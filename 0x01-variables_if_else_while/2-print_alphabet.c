e2e8390024a579f9aeb030bbd73034490ca8fdd3
Ade-nike17 <perfectnyke@gmail.com>
2023-02-19 16:29:19 -0800

    prints alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry value
+ *
+ * return - Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c) {
+		putchar(c);
+	}
+	return(0);
+}
