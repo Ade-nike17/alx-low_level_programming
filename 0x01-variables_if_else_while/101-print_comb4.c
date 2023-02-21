f0ed81af4ab9c5116c1555cc005240c8ed20c63c
Ade-nike17 <perfectnyke@gmail.com>
2023-02-20 16:53:27 -0800

    combination possibilities of three numbers

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int i, n, r;
+	for (i = 0; i < 10; i++)
+	{
+		for (n = i + 1; n < 10; n ++)
+		{
+			for (r = n + 1; r < 10; r++)
+			{
+				putchar(i + '0');
+				putchar(n + '0');
+				putchar(r + '0');
+				if (i != 7 || n != 8 || r != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
