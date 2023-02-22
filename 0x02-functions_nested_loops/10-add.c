b0c705045b59a0d45ac6ccfc16dc0c55aafb7984
Ade-nike17 <perfectnyke@gmail.com>
2023-02-22 08:57:11 -0800

    adds up two integers

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,12 +3,17 @@
 /**
  * add - function adds integer
  *
+ * num1: first integer
+ *
+ * num2: second integer
+ *
  * Return: result
  */
 
 int add(int num1, int num2)
 {
 	int res;
-	res = num1+ num2;
-	return(res);
+
+	res = num1 + num2;
+	return (res);
 }
