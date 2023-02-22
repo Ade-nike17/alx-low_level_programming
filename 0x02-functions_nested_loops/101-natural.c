ca203a32409b94db7bf8d4c98037db97de6630a8
Ade-nike17 <perfectnyke@gmail.com>
2023-02-22 11:49:17 -0800

    fixes trailing whitespace

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -2,7 +2,9 @@
 
 
 /**
- * main - entry point, function computes and prints the sum of all the multiples of 3 or 5 below 1024 
+ * main - entry point, function computes and prints the sum
+ *
+ * of all the multiples of 3 or 5 below 1024
  *
  * Return: int
  */
