e93dfe71d4036976ece5093b7f4f639cd30ae025
Ade-nike17 <perfectnyke@gmail.com>
2023-02-22 11:10:55 -0800

    naturak

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+.
+/**
+ * main - entry point, function computes and prints the sum of all the multiples of 3 or 5 below 1024 
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int mul = 0;
+	int sum = 0;
+
+	while(mul < 1024)
+	{
+		if (mul % 3 == 0 || mul % 5 ==  0)
+		{
+			sum += mul;
+		}
+		mul =+ 1;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
