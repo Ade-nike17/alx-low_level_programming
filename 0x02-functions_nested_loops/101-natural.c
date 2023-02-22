75a8666b16e7c94fec928308725e98fe0eb32190
Ade-nike17 <perfectnyke@gmail.com>
2023-02-22 11:44:40 -0800

    fixes compilation

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
-.
+
 /**
  * main - entry point, function computes and prints the sum of all the multiples of 3 or 5 below 1024 
  *
- * Return: 0
+ * Return: int
  */
 
 int main(void)
@@ -12,13 +12,13 @@ int main(void)
 	int mul = 0;
 	int sum = 0;
 
-	while(mul < 1024)
+	while (mul < 1024)
 	{
 		if (mul % 3 == 0 || mul % 5 ==  0)
 		{
 			sum += mul;
 		}
-		mul =+ 1;
+		mul += 1;
 	}
 	printf("%d\n", sum);
 	return (0);
