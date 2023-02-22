8a01c2fb5611a25facf3c1846fa8e60edba00800
Ade-nike17 <perfectnyke@gmail.com>
2023-02-22 03:29:04 -0800

    descriptions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,7 +5,7 @@
  *
  *@c: character to be checked
  *
- * return: 1 if character is lowercase and 0 if otherwise
+ * return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
