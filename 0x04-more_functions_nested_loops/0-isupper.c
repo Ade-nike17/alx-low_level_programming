0cd02eb5e959707d4d2e2c4733ec655442cd0796
Ade-nike17 <perfectnyke@gmail.com>
2023-02-23 17:50:29 -0800

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _isupper - function checks for uppercase characters
+ *
+ * @c: character to be checked
+ *
+ * Return: 0 (success)
+ */
+
+int _isupper(int c)
+{
+	if (c == 'A')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
