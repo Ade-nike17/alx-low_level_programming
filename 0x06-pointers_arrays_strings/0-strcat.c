ff8c2561ab93100eb28c5e3d31233b23b245c37d
Ade-nike17 <perfectnyke@gmail.com>
2023-03-01 11:59:23 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strcat - function concatenates two strings
+ *
+ * @dest: string 1
+ * @src: string 2 to be added to string 1
+ *
+ * Return: concatenated strings
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	/**
+	 * iterate over dest string to
+	 * move pointer to the end of string
+	 */
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+
+	/* copy dest string to dest string */
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+
+	/* adds a terminating null byte to dest string */
+	*ptr = '\0';
+
+	return (dest);
+
+}
