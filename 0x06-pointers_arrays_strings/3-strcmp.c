534adced05b33cd45808094e77cf75088fc2708e
Ade-nike17 <perfectnyke@gmail.com>
2023-03-02 12:19:38 -0800

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,29 +11,15 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int i = 0, cmp = s1[i] - s2[i];
-	
-	i++;
-	while (s1[i] != '\0' && (s2[i] != '\0'))
-	{
-		if (s1[i] < s2[i])
-		{
-			return (cmp);
-		}
+	int i = 0;
 
-		else if (s1[i] > s2[i])
-		{
-			return (cmp);
-		}
-		else
-		{
-			return (cmp);
-		}
-	}
-
-	if (s1[i] == '\0' || s2[i] == '\0')
+	while (s1[i] != '\0' || (s2[i] != '\0'))
 	{
-		return (0);
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
 	}
 	return (0);
 }
