98f76a9c880ea48101646998f1a3fdcd74b175e1
Ade-nike17 <perfectnyke@gmail.com>
2023-03-02 10:45:42 -0800

    strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strcmp - function compares two strings
+ *
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: result
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0, j = 0, cmp = s1[i] - s2[j];
+
+	if (s1[i] < s2[j])
+	{
+		return (cmp);
+	}
+
+	else if (s1[i] > s2[j])
+	{
+		return (cmp);
+	}
+	else
+	{
+		return (cmp);
+	}
+
+}
