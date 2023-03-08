65a0d08d2f4e7c2379bd17ea02c4db494a11864b
Ade-nike17 <perfectnyke@gmail.com>
2023-03-08 04:40:11 -0800

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * leet - function encodes strings
+ *
+ * @str: Array of string to be encoded
+ *
+ * Return: encoded string
+ */
+
+char *leet(char *str)
+{
+	char *ptr = str;
+	char leet_code[5] = {'4', '3', '0', '7', '1'};
+	char letters[5] = {'a', 'e', 'o', 't', 'l'};
+
+	int i, j;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (str[i] == letters[j] || str[i] == letters[j] -32)
+			{
+				*ptr = leet_code[j];
+				break;
+			}
+		}
+		ptr++;
+	}
+	return (str);
+}
