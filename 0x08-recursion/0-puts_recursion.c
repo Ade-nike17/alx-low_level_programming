e29c7635c82fb6cddc228a45862b3113f96ec5e4
Ade-nike17 <perfectnyke@gmail.com>
2023-03-09 05:40:50 -0800

    fixes new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * puts_recursion - prints a string
+ * _puts_recursion - prints a string
  *
  * @s: string to be printed
  *
@@ -10,13 +10,13 @@
 
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
 	{
+		_putchar('\n');
 		return;
 	}
 
 	_putchar(*s);
 	_puts_recursion(s + 1);
-	_putchar('\n');
 
 }
