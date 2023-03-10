3936e940ba0fcd0e0d72ff7e1d719cc746add977
Ade-nike17 <perfectnyke@gmail.com>
2023-03-10 03:59:54 -0800

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+/**
+ * main - prints its name
+ *
+ * Return: Always o
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+
+		printf("%s\n", argv[0]);
+
+	return (0);
+}
