4a1c0abbd79535daa920891fc9518c0022895b62
Ade-nike17 <perfectnyke@gmail.com>
2023-03-19 11:06:43 -0700

    fixes if expression

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	str1 = strlen(s1);
 	str2 = strlen(s2);
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL)
+		return ("");
+	if (s2 == NULL)
 		return ("");
 
 	if (n >= str2)
