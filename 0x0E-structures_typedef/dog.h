033883606f3efe45a22ee90c348cb75a78669eb1
Ade-nike17 <perfectnyke@gmail.com>
2023-03-20 14:45:04 -0700

    adds description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 
-/* new type */
+/**
+ * struct dog - Defines a new type called
+ * "dog" that contains a dog's name, age, and owner.
+ * @name: Pointer to a string containing the name of the dog.
+ * @age: Float value representing the age of the dog.
+ * @owner: Pointer to a string containing the
+ * name of the dog's owner.
+ *
+ * Description: This structure defines a new type
+ * called "dog", which contains information
+ * about a dog including its name, age, and owner.
+ * The name and owner are represented as pointers
+ * to strings, while the age is represented as
+ * a float value.
+ */
 
 struct dog
 {
