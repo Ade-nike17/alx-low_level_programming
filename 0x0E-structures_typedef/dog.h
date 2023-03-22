de3b148af515c1ba5182675d9d47d532e81b9fc2
Ade-nike17 <perfectnyke@gmail.com>
2023-03-22 15:06:07 -0700

    new type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -17,16 +17,13 @@
  * a float value.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
-/* Declare init_dog function */
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
 
 
 #endif /* DOG_H */
