99d3a04e4349f6258bfedc94931aced4eafc5a17
Ade-nike17 <perfectnyke@gmail.com>
2023-03-24 06:27:08 -0700

    adds if statement to check for null condition

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -14,6 +14,11 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
+	if (cmp == NULL || array == NULL)
+	{
+		return (-1);
+	}
+
 	if (size <= 0)
 	{
 		return (-1);
@@ -21,7 +26,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for  (i = 0; i < size; i++)
 	{
-		if (cmp(array[i]))
+		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
