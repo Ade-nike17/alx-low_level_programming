9ac1279d6426fafc467d4f523ca13cfc1bdf2760
Ade-nike17 <perfectnyke@gmail.com>
2023-04-04 09:22:39 -0700

    fixes if statement

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -19,12 +19,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	{
 		return (NULL);
 	}
-	else if (current != NULL)
+
+	for (i = 0; i < index && current != NULL; i++)
 	{
-		for (i = 0; i < index; i++)
-		{
-			current = current->next;
-		}
+		current = current->next;
+
 	}
 	return (current);
 }
