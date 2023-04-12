ced745e610e09cd2dca4d37a6e81b56208f282ee
Ade-nike17 <perfectnyke@gmail.com>
2023-04-12 00:44:34 -0700

    initializes bytes_written

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,39 +17,40 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	ssize_t bytes_written, bytes_read;
+	ssize_t bytes_read, bytes_written = 0;
 	char buffer[1024];
 
 	if (filename == NULL)
 	{
 		return (0);
 	}
-
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 	{
 		return (0);
 	}
-
-	bytes_read = read(fd, buffer, sizeof(buffer));
+	while (bytes_written < (ssize_t)letters &&
+			(bytes_read = read(fd, buffer, sizeof(buffer))) > 0)
+	{
+		if (bytes_written + bytes_read > (ssize_t)letters)
+		{
+			bytes_read = (ssize_t)letters - bytes_written;
+		}
+		if (write(STDOUT_FILENO, buffer, bytes_read) != bytes_read)
+		{
+			close(fd);
+			return (0);
+		}
+		bytes_written += bytes_read;
+	}
 	if (bytes_read == -1)
 	{
 		close(fd);
 		return (0);
 	}
-
-	if (letters > 0 && ((size_t)bytes_read > letters))
+	if (close(fd) == -1)
 	{
-		bytes_read = letters;
-	}
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1)
-	{
-		close(fd);
 		return (0);
 	}
-
-	close(fd);
 	return (bytes_written);
 }
