78c1771935fb5fdd7c3e4e938b2cb6b42728db8d
Ade-nike17 <perfectnyke@gmail.com>
2023-04-12 01:25:53 -0700

    Creates file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+
+/**
+ * create_file - function creates a file
+ *
+ * @filename: pointer to the file
+ * @text_content: a NULL terminated string to write the file
+ *
+ * Return: 1 if success and -1 if it failed
+ */
+
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+}
