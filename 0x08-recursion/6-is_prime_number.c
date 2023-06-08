422db336b3300f3ff37b416022cc09054c822cea
Ade-nike17 <perfectnyke@gmail.com>
2023-06-07 21:59:04 -0700

    prime

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <math.h>
+
+/**
+ * prime_helper - function checks if n is divisible by its sqrt
+ *
+ * @n: number to be checked for divisibility
+ * @divisor: number to divide n by
+ * 
+ * Return: 1 if prime or 0 if not prime
+ */
+
+
+int prime_helper(int n, int divisor)
+{
+	if (n < 2)
+		return (0);
+	if (divisor > sqrt(n))
+		return (1);
+	if (n % divisor == 0)
+		return (0);
+	return (prime_helper(n, divisor + 1));
+}
+
+
+
+/**
+ * is_prime_number - function checks if n is a prime 
+ *
+ * @n: number to be checked
+ * Return: value of prime helper
+ */
+
+int is_prime_number(int n)
+{
+	return (prime_helper(n, 2));
+}
