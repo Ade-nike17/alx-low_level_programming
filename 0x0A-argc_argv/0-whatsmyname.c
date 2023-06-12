eda6c92de7fef9a6f9764d415e739094f74a148f
Ade-nike17 <perfectnyke@gmail.com>
2023-06-12 11:28:14 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always 0 for success
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	int i = 0;
+
+	while (argv[i] != NULL)
+	{
+		printf("%s\n", *argv);
+		i++;
+	}
+	return (0);
+}
