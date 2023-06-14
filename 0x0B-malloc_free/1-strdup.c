9163b413d4e0361e596641cbdf68d32b503cf4af
Ade-nike17 <perfectnyke@gmail.com>
2023-06-14 06:49:44 -0700

    copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - function copies string
+ *
+ * @str: string to be copied
+ *
+ * Return: ptr to copied string
+ */
+
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int str_len;
+
+	if (str == NULL)
+		return (NULL);
+
+	str_len = strlen(str);
+	ptr = malloc(sizeof(char) * str_len);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	strcpy(ptr, str);
+
+	return (ptr);
+	free(ptr);
+}
+
+
