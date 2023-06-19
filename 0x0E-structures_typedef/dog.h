79951b2ce6161a89aa82a6fc4ef59d1d1c73c889
Ade-nike17 <perfectnyke@gmail.com>
2023-06-19 15:56:51 -0700

    adds description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Represents information about a dog
+ *
+ * @name: Pointer to the name of the dog
+ * @age: Age of the dog in years
+ * @owner: Pointer to the name of the dog's owner
+ */
 
 struct dog
 {
