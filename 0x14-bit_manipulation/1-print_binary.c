e994708d06296d305df5763f74db5468f62c99c3
Ade-nike17 <perfectnyke@gmail.com>
2023-07-07 04:53:27 -0700

    1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary rep of number
+ *
+ * @n: number to be converted to binary
+ *
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int binary_check;
+	int i = 63;
+	int count = 0;
+
+	while (i >= 0)
+	{
+		/* shift the current value of n by 'i' bit to the right */
+		binary_check = n >> i;
+		if (binary_check & 1)
+		{
+			_putchar('1');
+			count++;
+		}
+
+		else if (count)
+			_putchar('0');
+		i--;
+	}
+	if (!count)
+		_putchar('0');
+}
