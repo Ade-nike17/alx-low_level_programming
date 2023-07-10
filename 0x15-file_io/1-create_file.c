8229fe1504611b7df82b83db2b1d86406cc9500f
Ade-nike17 <perfectnyke@gmail.com>
2023-07-10 14:15:19 -0700

    function creates file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,8 +12,8 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	size_t length = 0;
-	ssize_t bytesWritten = 0;
+	size_t length;
+	ssize_t bytesWritten;
 
 	if (filename == NULL)
 		return (-1);
@@ -22,18 +22,21 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	length = strlen(text_content);
 	if (text_content != NULL)
 	{
 		while (text_content[length] != '\0')
 			length++;
-
+	
 		bytesWritten = write(fd, text_content, length);
 		if (bytesWritten == -1)
 		{
-			close(fd);
 			return (-1);
 		}
 	}
+	else if (text_content == NULL)
+		text_content = "";
+
 	close(fd);
 	return (1);
 }
