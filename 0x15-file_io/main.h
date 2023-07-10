366e3cd34616ef9be3e06055d4858af5f7244a00
Ade-nike17 <perfectnyke@gmail.com>
2023-07-10 12:18:27 -0700

    update libraries

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,11 @@
 #define MAIN_H
 #include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 /* prototype functions */
 ssize_t read_textfile(const char *filename, size_t letters);
