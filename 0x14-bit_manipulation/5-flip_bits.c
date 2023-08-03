31a381f585ca740521b4778ffd71b3ca296dd1ac
Ade-nike17 <perfectnyke@gmail.com>
2023-08-03 11:54:16 -0700

    flipped

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * flip_bits - function flip bits from one num
+ * to another
+ *
+ * @n: number to be flipped
+ * @m: to get to this number
+ *
+ * Return:number of bits flipped
+ */
+
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i = 63, bits_flipped = 0;
+	unsigned long int complement;
+	unsigned long int current;
+
+	complement = n ^ m;
+	while (i >= 0)
+	{
+		current = complement >> i;
+		if (current & 1)
+			bits_flipped++;
+
+		i--;
+	}
+	return (bits_flipped);
+}
