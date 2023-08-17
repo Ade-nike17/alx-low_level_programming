91fc4f2f96bf57cf63680dd1a2b4d3841030f454
Ade-nike17 <perfectnyke@gmail.com>
2023-08-17 12:01:24 -0700

    NULL

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -15,7 +15,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int count = 0;
 
 	current = *head;
-	if (*head == NULL || current == NULL)
+	if (*head == NULL)
 		return (-1);
 	if (index == 0)
 	{
