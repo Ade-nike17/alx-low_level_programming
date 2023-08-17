e3031f73a8bb2d83b9475601d793c81893358cfc
Ade-nike17 <perfectnyke@gmail.com>
2023-08-17 11:37:50 -0700

    betty

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -41,7 +41,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		if (current == NULL)
 		{
 			free(new_node);
-			return(NULL);
+			return (NULL);
 		}
 		new_node->next = current->next;
 		new_node->prev = current;
