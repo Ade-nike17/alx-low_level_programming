3cf3556b33d9e2fe17e5ce34ea6919044b1ffb8d
Ade-nike17 <perfectnyke@gmail.com>
2023-10-25 18:42:02 -0700

    Island Perimeter

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" This module contains a method island_perimeter"""
+
+def island_perimeter(grid):
+    """
+    Calculate the perimeter of the island described in the grid
+
+    Args:
+        grid:  A list of lists of integers where 0 represents a water zone and 1 represents a land zone.
+
+        return: The perimeter of the island
+    """
+
+    if not grid:
+        return 0
+
+    perimeter = 0
+    rows, cols = len(grid), len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                perimeter += 4 #start with 4 sides for each land cell
+
+                #check the adjacent cells (up, down, left, right)
+                if i > 0 and grid[i - 1][j] == 1:
+                    perimeter -= 1 #remove one side for adjacent land
+                if i < rows - 1 and grid[i + 1][j] == 1:
+                    perimeter -= 1
+                if j > 0 and grid[i][j - 1] == 1:
+                    perimeter -= 1
+                if j < cols - 1 and grid[i][j + 1] == 1:
+                    perimeter -= 1
+
+    return perimeter
