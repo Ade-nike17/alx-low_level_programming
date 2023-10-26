cac91fdcd7f8ee888453bc2619344e94d7ee8bd9
Ade-nike17 <perfectnyke@gmail.com>
2023-10-25 18:49:58 -0700

    fixes pycodestyle error

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 """ This module contains a method island_perimeter"""
 
+
 def island_perimeter(grid):
     """
     Calculate the perimeter of the island described in the grid
 
     Args:
-        grid:  A list of lists of integers where 0 represents a water zone and 1 represents a land zone.
+        grid:  A list of lists of integers where 0 represents a water zone
+            and 1 represents a land zone.
 
         return: The perimeter of the island
     """
@@ -20,11 +22,13 @@ def island_perimeter(grid):
     for i in range(rows):
         for j in range(cols):
             if grid[i][j] == 1:
-                perimeter += 4 #start with 4 sides for each land cell
+                # start with 4 sides for each land cell
+                perimeter += 4
 
-                #check the adjacent cells (up, down, left, right)
+                # check the adjacent cells (up, down, left, right)
                 if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 1 #remove one side for adjacent land
+                    # remove one side for adjacent land
+                    perimeter -= 1
                 if i < rows - 1 and grid[i + 1][j] == 1:
                     perimeter -= 1
                 if j > 0 and grid[i][j - 1] == 1:
